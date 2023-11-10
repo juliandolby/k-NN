8de6aee555c2c98fc25275621504fb7ed183aedc
Kavitha Srinivas <Kavitha.Srinivas@ibm.com>
2023-11-10 14:45:43 +0000

    test

---
+++ src/main/java/org/opensearch/knn/index/TestEngine.java
@@ -0,0 +1,7 @@
+import org.opensearch.knn.index.util.KNNEngine;
+
+public class TestEngine {
+    public static void main(String[] args) {
+        KNNEngine engine = KNNEngine.getEngine("pynn");
+    }
+}
